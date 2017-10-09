97bcf0ba4922d602f8f6eca13c6991fd7a6828e6
Mahmoud Yousif <mahn00b@users.noreply.github.com>
2017-10-09 16:24:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const Ganglion = require('openbci-ganglion').Ganglion;
-const ganglion = new Ganglion();
-ganglion.once('ganglionFound', (peripheral) => {
-  // Stop searching for BLE devices once a ganglion is found.
-  ganglion.searchStop();
-  ganglion.on('sample', (sample) => {
-    /** Work with sample */
-    console.log(sample.sampleNumber);
-    for (let i = 0; i < ganglion.numberOfChannels(); i++) {
-      console.log("Channel " + (i + 1) + ": " + sample.channelData[i].toFixed(8) + " Volts.");
-    }
-  });
-  ganglion.once('ready', () => {
-    ganglion.streamStart();
-  });
-  ganglion.connect(peripheral);
+const Cyton = require('openbci').Cyton;
+const ourBoard = new Cyton();
+ourBoard.connect(portName) // Port name is a serial port name, see `.listPorts()`
+  .then(() => {
+    ourBoard.on('ready',() => {
+      ourBoard.streamStart();
+      ourBoard.on('sample',(sample) => {
+        /** Work with sample */
+        for (let i = 0; i < ourBoard.numberOfChannels(); i++) {
+          console.log("Channel " + (i + 1) + ": " + sample.channelData[i].toFixed(8) + " Volts.");
+          // prints to the console
+          //  "Channel 1: 0.00001987 Volts."
+          //  "Channel 2: 0.00002255 Volts."
+          //  ...
+          //  "Channel 8: -0.00001875 Volts."
+          }
+        });
+    });
 });
-// Start scanning for BLE devices
-ganglion.searchStart();
